1b0159423717d96300f232c84663acbc6f3d7f56
obaya884 <taku3.884@gmail.com>
2023-11-11 23:32:18 +0900

    make item edit screen

---
+++ app/src/main/java/io/github/obaya884/favbasket/ui/screen/item_edit/ItemEditViewModel.kt
@@ -0,0 +1,37 @@
+package io.github.obaya884.favbasket.ui.screen.item_edit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.obaya884.favbasket.data.item.Item
+import io.github.obaya884.favbasket.domain.ItemRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ItemEditViewModel @Inject constructor(
+    private val itemRepository: ItemRepository
+) : ViewModel() {
+    private val _items = MutableStateFlow<List<Item>>(listOf())
+    val items: StateFlow<List<Item>> = _items
+
+    init {
+        viewModelScope.launch {
+            itemRepository.getAll()
+                .collect { items ->
+                    _items.value = items
+                }
+        }
+    }
+
+    fun addItem(newItemName: String) {
+        val newItem = Item(
+            name = newItemName,
+        )
+        viewModelScope.launch {
+            itemRepository.insert(newItem)
+        }
+    }
+}
