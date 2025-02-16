f136e352b6eaaf793f62bfe70c678363cd7374f0
obaya884 <taku3.884@gmail.com>
2025-02-17 05:07:22 +0900

    Roomのマイグレーションをリセット

--- app/src/main/java/io/github/obaya884/favbasket/data/AppDatabase.kt
+++ app/src/main/java/io/github/obaya884/favbasket/data/AppDatabase.kt
@@ -1,9 +1,10 @@
 package io.github.obaya884.favbasket.data
 
 import android.content.Context
-import androidx.room.*
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import io.github.obaya884.favbasket.data.category.Category
 import io.github.obaya884.favbasket.data.category.CategoryDao
 import io.github.obaya884.favbasket.data.item.Item
@@ -11,13 +12,10 @@ import io.github.obaya884.favbasket.data.item.ItemDao
 
 @Database(
     entities = [Item::class, Category::class],
-    version = 3,
+    version = 1,
     exportSchema = true,
-    autoMigrations = [
-        AutoMigration(from = 2, to = 3)
-    ]
 )
-@TypeConverters(InstantDateFormatStringConverter::class, DateLongConverter::class)
+@TypeConverters(InstantDateFormatStringConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
     abstract fun categoryDao(): CategoryDao
@@ -37,62 +35,9 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "app_database"
                 )
-                    .addMigrations(MIGRATION_1_2)
                     .build()
                 INSTANCE = instance
                 return instance
-            }
-        }
-
-        /*
-         * 日時を扱う列の型をLONGからTEXT("YYYY-MM-DD HH:MM:SS")に変更するマイグレーション
-         */
-        val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                // Itemテーブルのマイグレーション
-                db.execSQL("DROP INDEX IF EXISTS index_items_categoryId")
-                db.execSQL("ALTER TABLE items RENAME TO items_old")
-                db.execSQL(
-                    """
-                    CREATE TABLE items (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                        name TEXT NOT NULL,
-                        status TEXT NOT NULL,
-                        categoryId INTEGER,
-                        createdAt TEXT NOT NULL,
-                        updatedAt TEXT NOT NULL,
-                        FOREIGN KEY(categoryId) REFERENCES categories(id) ON DELETE SET NULL
-                    )
-                """.trimIndent()
-                )
-                db.execSQL(
-                    """
-                    INSERT INTO items (id, name, status, categoryId, createdAt, updatedAt)
-                    SELECT id, name, status, categoryId, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM items_old
-                """.trimIndent()
-                )
-                db.execSQL("CREATE INDEX index_items_categoryId ON items(categoryId)")
-                db.execSQL("DROP TABLE items_old")
-
-                // Categoryテーブルのマイグレーション
-                db.execSQL("ALTER TABLE categories RENAME TO categories_old")
-                db.execSQL(
-                    """
-                    CREATE TABLE categories (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                        name TEXT NOT NULL,
-                        createdAt TEXT NOT NULL,
-                        updatedAt TEXT NOT NULL
-                    )
-                """.trimIndent()
-                )
-                db.execSQL(
-                    """
-                    INSERT INTO categories (id, name, createdAt, updatedAt)
-                    SELECT id, name, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM categories_old
-                """.trimIndent()
-                )
-                db.execSQL("DROP TABLE categories_old")
             }
         }
     }
